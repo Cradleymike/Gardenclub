88426eafe5f1bf294e38ab760b1c3cb57cb14e7e
Mike <hollismike@hotmail.com>
2020-06-06 21:51:06 +0100

    Updated requirements with potential change Updated requirements with potential change

--- setup.py
+++ setup.py
@@ -12,14 +12,26 @@ class Classes :
         self.sixth_prize = sixth_prize
         self.section = section
 
-    def update(self):
+    def update_desc(self):
         print(f"Current description is :{self.description}.")
         new_description = input ("what is the new description?:")
         self.description = new_description
 
+    def update_prize(self):
+        print(f"Current prize money \n first:{self.first_prize}\n second:{self.second_prize}")
+        self.first_prize = float(input("enter new first prize:"))
+        self.second_prize = float(input("Enter new second prize"))
 
+
+#testing of the class
 c1 = Classes(1,"large onion",30,20,10,8,5,3,"Vegetable")
 
 print(c1.description)
-c1.update()
+c1.update_desc()
 print(c1.description)
+
+print (c1.first_prize)
+print (c1.second_prize)
+c1.update_prize()
+print (c1.first_prize)
+print (c1.second_prize)
