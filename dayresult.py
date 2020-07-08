7d912c4a984739db04a68d105c0cbe46d4263518
Mike <hollismike@hotmail.com>
2020-07-08 15:12:31 +0100

    day results top three selected

--- dayresult.py
+++ dayresult.py
@@ -31,6 +31,21 @@ def populate_list():
 def add_details():
     pass
 
+def first_details():
+    global first_text
+    global selected_item
+    first_text.set(selected_item[0])
+
+def second_details():
+    global second_text
+    global selected_item
+    second_text.set(selected_item[0])
+
+def third_details():
+    global third_text
+    global third_item
+    third_text.set(selected_item[0])
+
 def select_item(event):
     try:
         global selected_item
@@ -54,8 +69,30 @@ number_label.grid(row=1, column=0, sticky = W)
 number_entry = Entry(app3, width = 10, textvariable= number_text)
 number_entry.grid(row = 1,column=1, sticky = W)
 
+#first
+first_text = StringVar()
+first_label = Label(app3, text='First',  pady = 5)
+first_label.grid(row=2, column=0, sticky = W)
+first_entry = Entry(app3, width = 10, textvariable= first_text)
+first_entry.grid(row = 2,column=1, sticky = W)
+
+#second
+second_text = StringVar()
+second_label = Label(app3, text='Second',  pady = 5)
+second_label.grid(row=3, column=0, sticky = W)
+second_entry = Entry(app3, width = 10, textvariable= second_text)
+second_entry.grid(row = 3,column=1, sticky = W)
+
+#third
+third_text = StringVar()
+third_label = Label(app3, text='Third',  pady = 5)
+third_label.grid(row=4, column=0, sticky = W)
+third_entry = Entry(app3, width = 10, textvariable= third_text)
+third_entry.grid(row = 4,column=1, sticky = W)
+
+
 #Listbox for the existing Exhibitors
-exhibitor_list = Listbox(app3, height=8, width = 50, border =0)
+exhibitor_list = Listbox(app3, height=5, width = 50, border =0)
 exhibitor_list.grid(row=5, column = 0, columnspan = 3, rowspan = 6, pady=20, padx = 20)
 
 #listbox scrollbar
@@ -68,6 +105,17 @@ scrollbar.configure(command = exhibitor_list.yview)
 
 #Bind select item
 exhibitor_list.bind('<<ListboxSelect>>',select_item)
+
+
+#Buttons
+first_btn = Button(app3, text='First', width = 12, command = first_details)
+first_btn.grid(row = 13, column = 0, pady = 20)
+
+second_btn = Button(app3, text='Second', width = 12, command = second_details)
+second_btn.grid(row = 13, column = 1)
+
+third_btn = Button(app3, text='Third', width = 12, command = third_details)
+third_btn.grid(row = 13, column = 2)
 
 add_btn = Button(app3, text='Add', width = 12, command = add_details)
 add_btn.grid(row = 14, column = 0, pady = 20)
